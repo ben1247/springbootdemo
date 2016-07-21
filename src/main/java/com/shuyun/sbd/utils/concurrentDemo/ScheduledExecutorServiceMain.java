36e206f039c17c7d3962d1ddd4c53e0408759e00
张 悦 <yue.zhang@shuyun.com>
2016-07-21 17:34:26 +0800

    scheduled executor service demo

---
+++ src/main/java/com/shuyun/sbd/utils/concurrentDemo/ScheduledExecutorServiceMain.java
@@ -0,0 +1,31 @@
+package com.shuyun.sbd.utils.concurrentDemo;
+
+import java.util.concurrent.*;
+
+/**
+ * Component:  定时执行者服务 ScheduledExecutorService
+ * Description:
+ * Date: 16/7/18
+ *
+ * @author yue.zhang
+ */
+public class ScheduledExecutorServiceMain {
+
+    public static void main(String [] args) throws ExecutionException, InterruptedException {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+
+        ScheduledFuture scheduledFuture = scheduledExecutorService.schedule(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                System.out.println("Executed !");
+
+                return "Called";
+            }
+        },3, TimeUnit.SECONDS);
+
+        System.out.println("result = " + scheduledFuture.get());
+
+        scheduledExecutorService.shutdown();
+    }
+
+}
