ef123e82fb3047f391ca0b1f6eb120a4bdaf3b48
张 悦 <yue.zhang@shuyun.com>
2016-08-15 11:35:29 +0800

    echo server modify

--- src/main/java/com/shuyun/sbd/utils/netty/echo/EchoServer.java
+++ src/main/java/com/shuyun/sbd/utils/netty/echo/EchoServer.java
@@ -1,9 +1,17 @@
 package com.shuyun.sbd.utils.netty.echo;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 
 /**
@@ -15,13 +23,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  */
 public class EchoServer {
 
-    private int port;
-
-    public EchoServer(int port) {
-        this.port = port;
-    }
-
-    public void run() throws Exception{
+    public void run(int port) throws Exception{
 
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -32,21 +34,33 @@ public class EchoServer {
 
             b.group(bossGroup,workerGroup)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer() {
-
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(Channel channel) throws Exception {
-                        channel.pipeline().addLast(new EchoServerHandler());
-                    }
-                })
-                .option(ChannelOption.SO_BACKLOG,128)
-                .childOption(ChannelOption.SO_KEEPALIVE,true);
+                    protected void initChannel(SocketChannel channel) throws Exception {
+                        ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
 
+                        // DelimiterBasedFrameDecoder的第一个参数1024表示单条消息的最大长度，当达到该长度后仍然没有查找到分隔符，就抛出TooLongFrameException异常，
+                        // 防止由于异常码流缺失分隔符导致的内存溢出，这是netty解码器的可靠性保护。第二个参数就是分隔符缓冲对象。
+                        channel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter)); // 作用是防止粘包和拆包
+
+                        // FixedLengthFrameDecoder是固定长度解码器，它能够按照制定的长度对消息进行自动解码，开发者不需要考虑TC的粘包和拆包问题。
+//                        channel.pipeline().addLast(new FixedLengthFrameDecoder(20));
+
+                        channel.pipeline().addLast(new StringDecoder()); // 作用是将byteBuf解码成字符串对象
+                        channel.pipeline().addLast(new EchoServerHandler()); // 接收到的msg消息就是解码后的字符串对象
+                    }
+                });
+
+            // 绑定端口，同步等待成功
             ChannelFuture f = b.bind(port).sync();
 
+            // 等待服务端监听端口关闭
             f.channel().closeFuture().sync();
 
         }finally {
+            // 优雅退出，释放线程池资源
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
@@ -54,6 +68,6 @@ public class EchoServer {
     }
 
     public static void main(String [] args) throws Exception {
-        new EchoServer(8999).run();
+        new EchoServer().run(8999);
     }
 }
--- src/main/java/com/shuyun/sbd/utils/netty/echo/EchoServerHandler.java
+++ src/main/java/com/shuyun/sbd/utils/netty/echo/EchoServerHandler.java
@@ -1,5 +1,7 @@
 package com.shuyun.sbd.utils.netty.echo;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
@@ -12,19 +14,20 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class EchoServerHandler extends ChannelHandlerAdapter{
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // ChannelHandlerContext对象提供了许多操作，使你能够触发各种各样的I/O事件和操作。
-        // 这里我们调用了write(Object)方法来逐字地把接受到的消息写入。
-        // 请注意不同于DISCARD的例子我们并没有释放接受到的消息，这是因为当写入的时候Netty已经帮我们释放了。
-        ctx.write(msg);
-    }
+    int counter = 0;
 
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        // ctx.write(Object)方法不会使消息写入到通道上，他被缓冲在了内部，你需要调用ctx.flush()方法来把缓冲区中数据强行输出。
-        // 或者你可以用更简洁的cxt.writeAndFlush(msg)以达到同样的目的。
-        ctx.flush();
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        // 如下代码用于使用DelimiterBasedFrameDecoder 解码器
+        String body = (String)msg;
+        System.out.println("This is " + ++counter + " times receive client : [" + body + "]");
+        body += "$_";
+        ByteBuf buf = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(buf);
+
+        // 如下代码用于使用FixedLengthFrameDecoder 解码器, client 可以使用 telnet 127.0.0.1 8999 来发送消息
+//        System.out.println("Receive client: [" + msg + "]");
     }
 
     @Override
